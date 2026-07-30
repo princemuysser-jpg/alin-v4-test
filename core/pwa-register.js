880669988bdf878c1c2051cbe19f1fa6483269e8
princemuysser-jpg <prince.muysser@gmail.com>
2026-07-30 07:25:30 +0300

    Prevent old interface cache

--- core/pwa-register.js
+++ core/pwa-register.js
@@ -7,7 +7,7 @@
 
   window.addEventListener('load',async()=>{
     try{
-      const registration=await navigator.serviceWorker.register('./service-worker.js?v=4.0.0',{scope:'./',updateViaCache:'none'});
+      const registration=await navigator.serviceWorker.register('./service-worker.js?v=4.0.12',{scope:'./',updateViaCache:'none'});
       registration.addEventListener('updatefound',()=>{
         const worker=registration.installing;
         if(!worker)return;
