1171608ebd49d7b5a4053fbc9a39b69273724fc2
princemuysser-jpg <prince.muysser@gmail.com>
2026-08-01 16:20:01 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION='alin-v4.1.6-protected-print';
+const VERSION='alin-v4.1.6-protected-print-contrast';
 const STATIC_CACHE=`${VERSION}-static`;
 const RUNTIME_CACHE=`${VERSION}-runtime`;
 
