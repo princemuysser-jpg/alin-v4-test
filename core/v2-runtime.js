15124bc599b01031ab2111dbf158315c828e3988
princemuysser-jpg <prince.muysser@gmail.com>
2026-08-04 01:50:50 +0300

    Delete core directory

--- core/v2-runtime.js
+++
@@ -1,5 +0,0 @@
-(()=>{
-  'use strict';
-  window.addEventListener('error',e=>console.error('[ALIN v2 runtime]',e.error||e.message));
-  window.addEventListener('unhandledrejection',e=>console.error('[ALIN v2 promise]',e.reason));
-})();
