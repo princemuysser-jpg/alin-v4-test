910e6e43141b0289c9c7bafc6e4be8b778e0c678
princemuysser-jpg <prince.muysser@gmail.com>
2026-08-22 23:43:57 +0300

    5.6.0

--- alin-config.js
+++ alin-config.js
@@ -1,7 +1,7 @@
 // منصة آلين v4.2.0 Stable — Alin Platform.
 window.ALIN_CONFIG=Object.freeze({
   version:'4.2.0',
-  assetVersion:'4.2.0-ui14-tablet-layout',
+  assetVersion:'4.2.0-notification-draft-final-20260822',
   desktopPage:'./store-desktop.html',
   mobilePage:'./store-mobile.html',
   tabletPage:'./store-tablet.html',
