36b6767dbf57fbdf1bf0a628a492bd8c5551e801
princemuysser-jpg <prince.muysser@gmail.com>
2026-09-08 22:20:17 +0300

    feat: add ALIN Business config

---
+++ flutter_business/lib/core/business_config.dart
@@ -0,0 +1,7 @@
+class BusinessConfig {
+  static const supabaseUrl = 'https://dgaikazhbtyjmswpyvrl.supabase.co';
+  static const supabasePublishableKey = 'sb_publishable_HjVoise8mRYVeMeBaM9pxw_oxf_mLp3';
+  static const appName = 'آلين للأعمال';
+  static const appSubtitle = 'إدارة أعمالك من مكان واحد';
+  static const currency = 'د.ع';
+}
